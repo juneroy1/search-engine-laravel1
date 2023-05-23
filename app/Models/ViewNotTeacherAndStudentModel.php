fa5bf842f3ae718028f6a79e03939cadc5af4ec8
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-24 00:36:19 +0800

    feat: added models for views mysql for students, teachers, and non-teachers and non-students

---
+++ app/Models/ViewNotTeacherAndStudentModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ViewNotTeacherAndStudentModel extends Model
+{
+    use HasFactory;
+     protected $table = 'view_notteachers_nostudents_user_data';
+}
