f3e0ea07e137d7c8b20a29c2693a1a65dc40efdb
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-23 23:49:37 +0800

    feat: added user id creator and number of times update migration

---
+++ database/migrations/2023_05_14_142252_add-column-number-of-times-update.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnNumberOfTimesUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->integer('number_of_times_update')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
