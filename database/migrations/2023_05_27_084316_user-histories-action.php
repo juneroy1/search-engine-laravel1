8319e67bdc21428a2486d4e849c593146af32ad9
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-27 22:51:59 +0800

    feat: added user histories action migration

---
+++ database/migrations/2023_05_27_084316_user-histories-action.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserHistoriesAction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_histories_action', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('who_change_id')->nullable();
+            $table->foreign('who_change_id')->references('id')->on('users');
+            $table->string('action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_histories_action');
+    }
+}
